1aa3d70b650a1e6e7b01ffcd629e5478a5a51898
swg <anarchipur@googlemail.com>
2018-12-02 22:52:33 +0100

    Requirede Params defined

---
+++ RESTService/Models/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RESTService.Models
+{
+    public class Document
+    {
+        public string OBID { get; set; }
+        public string Title { get; set; }
+
+        [ForeignKey("OBID")]
+        public Part Part { get; set; }
+    }
+}
--- RESTService/Models/Part.cs
+++ RESTService/Models/Part.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace RESTService.Models
 {
     public class Part
     {
         public string OBID { get; set; }
+
+        [Required]
         public string PartNumber { get; set; }
         public string PCI { get; set; }
         public string UsageCondition { get; set; }
